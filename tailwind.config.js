55cdc9df8391a6e13f81fd50eb07045079a90f7d
Cykotech <cykotech.development@yahoo.com>
2024-12-25 18:41:42 -0500

    Initialize Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#240e1f",
+      secondary: "#f0b052",
+      accent: "#e96932",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
